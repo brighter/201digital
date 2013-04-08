d9ceb6d368aed9176b56453e6300eb0694d78c39
Richard O'flynn <ispyinternet@yahoo.co.uk>
2013-04-08 19:21:49 +0100

    add www2 folder

--- www2/index.php
+++ www2/index.php
@@ -4,8 +4,40 @@ html_header('','');
 ?>
 <div id='breadcrumb'><span class='label'>You Are Here: </span><span class='trail'>Home</span> > <span class='trail current'>About Us</span></div>
 <div id='power-content'>
+  
+  <div id='slide-buttons'>
+  <div id='button1' class='slide-button active'></div>
+  <div id='button2' class='slide-button'></div>
+  <div id='button3' class='slide-button'></div>
+  <div id='button4' class='slide-button'></div>
+  </div>
+
+  <div id='slide4' class='yellow slide hidden'>
+  <br /><br /><br />
+  <h1 class='subtitle'></h1>
+  <h1 class='title'>welcome</h1>
+  </div>
+  
+  <div id='slide1' class='purple slide hidden'>
+  <br /><br /><br />
   <h1 class='subtitle'>digital</h1>
   <h1 class='title'>brand building</h1>
+  </div>
+
+  <div id='slide2' class='red slide hidden'>
+  <br /><br /><br />
+  <h1 class='subtitle'>on-line</h1>
+  <h1 class='title'>marketing strategies</h1>
+  </div>
+
+  <div id='slide3'  class='blue slide hidden'>
+  <br /><br /><br />
+  <h1 class='subtitle'>engaging</h1>
+  <h1 class='title'>social media</h1>
+  </div>
+
+
+
 </div>
 
 <div id='main-content'>
@@ -15,6 +47,7 @@ html_header('','');
   <p>You are about to witness, the strength of street knowledge</p>
   </div>
   </div>
+  <script language='javascript' src='js/slideshow.js'></script>
 <?php
 
 html_footer() ?>
